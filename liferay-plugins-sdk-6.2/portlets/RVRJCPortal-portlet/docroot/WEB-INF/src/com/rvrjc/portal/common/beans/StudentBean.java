0365ee96b5b118b41f118f29361e1664de204cb4
SureshYadagiri <suresh.yadagiri@gmail.com>
2017-04-14 11:42:33 -0400

    Added common bean StudentBean

---
+++ liferay-plugins-sdk-6.2/portlets/RVRJCPortal-portlet/docroot/WEB-INF/src/com/rvrjc/portal/common/beans/StudentBean.java
@@ -0,0 +1,23 @@
+package com.rvrjc.portal.common.beans;
+
+public class StudentBean {
+
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	
+}
