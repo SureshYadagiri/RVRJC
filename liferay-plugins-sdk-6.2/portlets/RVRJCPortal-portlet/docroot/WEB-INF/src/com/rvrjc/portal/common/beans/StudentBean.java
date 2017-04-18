05ee612119c36002168acd755a8de7e731389bfa
SureshYadagiri <suresh.yadagiri@gmail.com>
2017-04-18 11:13:03 -0400

    Added middleName

--- liferay-plugins-sdk-6.2/portlets/RVRJCPortal-portlet/docroot/WEB-INF/src/com/rvrjc/portal/common/beans/StudentBean.java
+++ liferay-plugins-sdk-6.2/portlets/RVRJCPortal-portlet/docroot/WEB-INF/src/com/rvrjc/portal/common/beans/StudentBean.java
@@ -4,7 +4,16 @@ public class StudentBean {
 
 	private String firstName;
 	private String lastName;
+	private String middleName;
 	
+
+	
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
